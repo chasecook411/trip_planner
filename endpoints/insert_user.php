7ab1637a4fe84d3ac477277661458b0bb827bc57
dmcoomes <damiancoomes@gmail.com>
2017-11-05 19:55:46 -0600

    query string error

--- endpoints/insert_user.php
+++ endpoints/insert_user.php
@@ -9,13 +9,17 @@ if (isset($_POST['f_name']) && isset($_POST['l_name']) && isset($_POST['email'])
     $password = $_POST['password'];
 
     // verify that email does not already exist
-    $query = 'select email from users where email = "' . $email '";';
+    $query = 'select email from users where email = "' . $email . '";';
 
-    // if result of query is greater than/equal to 1, then we need to err. 
-
+    // if result of query is greater than/equal to 1, then we need to err.
+    if ($query != null) {
+       echo "This email address has already been registered with an account.";
+    }
     //otherwise, start insertion
-    $query = 'insert into users values(NULL, "' . $f_name . '","'.$l_name . '","' . $email . '","' . $password. '");';
-    echo $query;
+    else {
+        $query = 'insert into users values(NULL, "' . $f_name . '","'.$l_name . '","' . $email . '","' . $password . '");';
+        echo $query;
+    }
 }
 
 ?>
