fb7a5e43c223bee692210da97d07377a56761d9f
Daniel Linn <dalinn@memphis.edu>
2017-11-07 10:07:42 -0600

    Added my_trips.php

---
+++ my_trips.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<head>
+	<title>My Trips</title>
+	<script src="jquery-3.2.1.min.js"></script>
+
+
+
+
+</head>
+<body>
+	<div id="navigation">//placeholder for navigation bar
+	</div></br>
+
+	<div id="tripsList">
+		
