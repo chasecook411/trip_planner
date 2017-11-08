6baf5b055ec2eb07fba36d1b27376c73f47a46d2
Daniel Linn <dalinn@memphis.edu>
2017-11-08 00:09:26 -0600

    modified connection string for database used in local setting and added very basic trip list.

--- my_trips.php
+++ my_trips.php
@@ -1,18 +1,54 @@
 <?php
 
+$conn = new mysqli('localhost', 'root', 'root', 'lost_db');
+if ($conn->connect_error) {
+	die("Could not connect:" . $conn->connect_error);
+}
+
+$userid = $_GET['userid'];
 ?>
 
-<head>
-	<title>My Trips</title>
-	<script src="jquery-3.2.1.min.js"></script>
+<html>
+	<head>
+		<title>My Trips</title>
+		<script src="jquery-3.2.1.min.js"></script>
+		<script>
+			var tripsList = [];
+
+			function getTrips() {
 
 
 
+			}
+		</script>
 
-</head>
-<body>
-	<div id="navigation">//placeholder for navigation bar
-	</div></br>
 
-	<div id="tripsList">
-		
+
+	<style>
+		#trips {
+			float: left;
+			width: 25%;
+		}
+	</style>
+
+	</head>
+	<body>
+		<div id="navigation">
+				</div></br>
+
+
+		<div id="trips">
+			<h3>My Trips</h3>
+		<?php
+			$query = 'select * from trips where user_id = "' . $userid . '";';
+			$result = $conn->query($query) or die('Error querying database.');
+
+			while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+				echo $row['trip_name'] . ' on ' . $row['day'] . '</br>';
+			}
+			$conn->close();
+		?>		
+		</div>
+
+	</body>
+</html>
